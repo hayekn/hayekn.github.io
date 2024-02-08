7e480d3ab3ceb58a3f6e48acb4573291d1ddea80
Nicholas Hayek <hayekn1@outlook.com>
2024-02-07 22:47:32 -0500

    screw onedrive we got hardcopies

---
+++ scripts/hardcopy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln "../../OneDrive - McGill University/School Documents/$SUBJ/$CODE/$1"  "../tex/$SUBJ/$CODE/$1"
+
+exit 0
---
+++ scripts/mhardcopy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln "../../OneDrive - McGill University/School Documents/$1/$2/$3"  "../tex/$1/$2/$3"
+
+exit 0
