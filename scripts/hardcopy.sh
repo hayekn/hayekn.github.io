d6ed1978ea5cd31bfaec0415fcc8d3a4973eff6c
Nicholas Hayek <hayekn1@outlook.com>
2024-02-12 17:14:19 -0500

    remove 3

--- scripts/hardcopy.sh
+++ scripts/hardcopy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $1 = help || $# = 0 ]]
+then
+	echo -e "Usage: takes 1 argument, fileName; requires 2 global variables, SUBJ and CODE\nFinds School Documents/SUBJ/CODE/fileName and creates hardlink to tex/SUBJ/CODE/fileName"
+	exit 0
+fi
+
 ln "../../OneDrive - McGill University/School Documents/$SUBJ/$CODE/$1"  "../tex/$SUBJ/$CODE/$1"
 
 exit 0
---
+++ scripts/refresh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm "../tex/$SUBJ/$CODE/$1"
+ln "../../OneDrive - McGill University/School Documents/$SUBJ/$CODE/$1"  "../tex/$SUBJ/$CODE/$1"
+
+exit 0
