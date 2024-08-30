de7a93c6fac95b6cbe90b05ff4ca9f6322df1be1
Nicholas Hayek <hayekn1@outlook.com>
2024-08-30 10:18:52 -0400

    refresh Algebra 3 Notes.pdf

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-rm "../tex/$1/$2/$3"
-ln "../../OneDrive - McGill University/School Documents/$1/$2/$3"  "../tex/$1/$2/$3"
+GITREPO="/Users/nicholashayek/hayekn.github.io"
+SCHOOLDOCS="/Users/nicholashayek/OneDrive - McGill University/School Documents" 
 
+if test ! -d "$GITREPO/tex/$1/$2" ; then
+  mkdir $GITREPO/tex/$1/$2
+fi
+
+rm "$GITREPO/tex/$1/$2/$3"
+
+ln "$SCHOOLDOCS/$1/$2/$3"  "$GITREPO/tex/$1/$2/$3"
+
+cd $GITREPO
 git commit -am "refresh $3"
 git push origin main
 
