d721d22cb2454ded4a970659c4768e16fc725afa
Nicholas Hayek <hayekn1@outlook.com>
2025-12-30 22:33:25 -0500

    tikz advice

--- output.php
+++
@@ -1,9 +0,0 @@
-<?php
-$path = 'stuff.txt';
-if (isset($_POST['submission'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['submission'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
-}
-?>
